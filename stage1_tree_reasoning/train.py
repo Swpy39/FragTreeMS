9b825e057f66b06ef6d1f2ac433d2d66b45ed491
Hongyan Sun <131747313+Swpy39@users.noreply.github.com>
2026-09-08 12:55:34 +0800

    Update train.py

--- stage1_tree_reasoning/train.py
+++ stage1_tree_reasoning/train.py
@@ -89,7 +89,7 @@ from torch.optim import AdamW
 from torch.utils.data import DataLoader, Dataset, DistributedSampler
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-from fitgraph_ion_recall_common_v2 import (
+from fittree import (
     build_chat_prompt,
     build_fitgraph,
     build_repair_chat_prompt,
